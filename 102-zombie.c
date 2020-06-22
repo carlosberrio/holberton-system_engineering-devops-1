073f645cccab0e0962c1419d9fca6878a30d3b1d
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-06-22 23:09:29 +0000

    Adding 102 task

---
+++ 102-zombie.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - Runs an infinite loop
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates five child processes
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 5; c++)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			return (0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
